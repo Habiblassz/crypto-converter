b5ae483e5a836fac62afa05adb2eef687cdc9116
Lawal Habib <lasszcodes@gmail.com>
2025-12-23 12:17:23 +0100

    feat: define TypeScript interfaces for conversion data

---
+++ src/types/converter.ts
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+export type ConversionType =
+	| "crypto-to-cash"
+	| "cash-to-crypto"
+	| "crypto-to-loan";
+
+export interface Currency {
+	code: string;
+	name: string;
+	icon?: string;
+	flag?: string;
+}
+
+export interface PaymentOption {
+	value: string;
+	label: string;
+	icon: string;
+}
