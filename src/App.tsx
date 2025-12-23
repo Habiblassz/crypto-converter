7ef90087c11cb9e11eea6260a0a943e949cc063d
Lawal Habib <lasszcodes@gmail.com>
2025-12-23 12:21:25 +0100

    feat: scaffold main responsive layout

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CryptoConverter } from "./components/crypto-converter";
+
+function App() {
+	return (
+		<main className="min-h-screen bg-[#1a1a1a] flex items-start md:items-center justify-center p-4">
+			<CryptoConverter />
+		</main>
+	);
+}
+
+export default App;
